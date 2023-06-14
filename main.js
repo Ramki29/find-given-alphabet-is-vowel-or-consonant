a8937e4474dfd5c2dfeeccd340298a41ca1252eb
Ramki29 <ramkie7010@gmail.com>
2023-06-14 15:54:40 +0530

    add find-given-alphabet-is-vowel-or-consonant

---
+++ main.js
@@ -0,0 +1,9 @@
+let value=prompt("enter any alphabet");
+if(value=="a" || value=="e" || value=="i" || value=="o" || value=="u")
+{
+	console.log(" vowel");
+}
+else
+{
+	console.log("consonant");
+}
